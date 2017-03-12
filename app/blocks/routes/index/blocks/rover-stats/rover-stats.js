7995457cfb7aab6b001a4bec272f015680b21eba
Seva Arkhangelskiy <cubadacapo@gmail.com>
2017-03-12 17:59:27 -0400

    Display rover position when it stops moving

---
+++ app/blocks/routes/index/blocks/rover-stats/rover-stats.js
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+
+import BaseComponent from "base/component";
+
+export default BaseComponent.extend({
+
+    name: "rover-stats",
+
+
+
+    /**
+     * Attributes
+     */
+
+    /**
+     * @property {App.Rover}
+     * @readonly
+     */
+    rover: null
+
+});
